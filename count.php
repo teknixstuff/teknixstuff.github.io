5b500d70cdbdd8d61faf44ecb9e7b6678d162ada
Tech Stuff <teknixstuff@gmail.com>
2025-05-18 18:10:07 +0100

    update

--- count.php
+++ count.php
@@ -1,3 +1,7 @@
 <?php
+header('Expires: Sun, 01 Jan 2000 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
 $file = '/var/www/counts/' . preg_replace('/[^a-zA-Z0-9_-]/s', '_', $_GET["id"]) . '.txt';
 file_put_contents($file, file_get_contents($file) + 1);
